57780e5e039c90d5937ff986bcf96e3277e7a391
Carl-Johnsons <nguyenletaiduc2709@gmail.com>
2024-01-07 17:47:20 +0700

    done adding owl carousel to the popular item container

--- js/backgroundCircle.js
+++ js/backgroundCircle.js
@@ -1,40 +1,55 @@
-
 document.addEventListener("DOMContentLoaded", () => {
     main();
 });
 
 function main() {
-    const BACKGROUND_CIRCLE = "background-circle-";
-    const DEFAULT_BACKGROUND_CIRCLE = -1;
+    const BACKGROUND_CIRCLE = ["background-min-circle-", "background-max-circle-"];
+    const DEFAULT_BACKGROUND_CIRCLE = 0;
+    const BACKGROUND_CIRCLE_COLOR = "background-circle-color-";
+    const DEFAULT_BACKGROUND_CIRCLE_COLOR = "red";
     const INNER_CIRCLE_BORDER_WIDTH = "inner-circle-border-width-";
     const DEFAULT_BORDER_WIDTH = "10px";
     const INNER_CIRCLE_BORDER_COLOR = "inner-circle-border-color-";
     const DEFAULT_BORDER_COLOR = "black";
-    let backgroundCircles = document.querySelectorAll(`[class*='${BACKGROUND_CIRCLE}']`);
+    let backgroundCircles = document.querySelectorAll(BACKGROUND_CIRCLE.map(prefix => `[class*='${prefix}']`).join(','));
 
     for (let backgroundCircle of backgroundCircles) {
-        minValue = Math.min(backgroundCircle.offsetWidth, backgroundCircle.offsetHeight);
 
         let eleClassList = backgroundCircle.classList;
-        console.log(eleClassList)
-        let innerCircles = getClassValue(eleClassList, BACKGROUND_CIRCLE, DEFAULT_BACKGROUND_CIRCLE);
+        let innerCircles = getClassValue(eleClassList, DEFAULT_BACKGROUND_CIRCLE, ...BACKGROUND_CIRCLE);
         // Edge case: if not found
         if (innerCircles === DEFAULT_BACKGROUND_CIRCLE) {
             continue;
         }
-        let innerCircleBorderWidth = getClassValue(eleClassList, INNER_CIRCLE_BORDER_WIDTH, DEFAULT_BORDER_WIDTH);
+        let backgroundCircleColor = getClassValue(eleClassList, DEFAULT_BACKGROUND_CIRCLE_COLOR, BACKGROUND_CIRCLE_COLOR);
+        let innerCircleBorderWidth = getClassValue(eleClassList, DEFAULT_BORDER_WIDTH, INNER_CIRCLE_BORDER_WIDTH);
         innerCircleBorderWidth = parseInt(innerCircleBorderWidth.match(/\d+/g));
 
-        let innerCircleBorderColor = getClassValue(eleClassList, INNER_CIRCLE_BORDER_COLOR, DEFAULT_BORDER_COLOR);
+        let innerCircleBorderColor = getClassValue(eleClassList, DEFAULT_BORDER_COLOR, INNER_CIRCLE_BORDER_COLOR);
+        let isMin = false;
+        eleClassList.forEach(className => {
+            if (className.startsWith(BACKGROUND_CIRCLE[0])) {
+                console.log(className);
+                isMin = true;
+            }
+        })
 
-        minValue -= (innerCircleBorderWidth * 2);
-        let margin = (minValue / innerCircles);
-        let innerCircleValue = minValue;
+        console.log({ isMin });
+        let value;
+        value = isMin ?
+            Math.min(backgroundCircle.offsetWidth, backgroundCircle.offsetHeight)
+            : Math.max(backgroundCircle.offsetWidth, backgroundCircle.offsetHeight);
+        value -= (innerCircleBorderWidth * 2);
+        let margin = (value / innerCircles);
+        let innerCircleValue = value;
+        // Set up the background
+        backgroundCircle.style.overflow = "hidden";
+        backgroundCircle.style.backgroundColor = backgroundCircleColor;
         for (let i = 0; i < innerCircles; i++) {
             let div = generateEle("div", "inner-circle pos-absolute position-center");
             div.style.width = `${innerCircleValue}px`;
             div.style.height = `${innerCircleValue}px`;
-            div.style.borderWidth = `${innerCircleBorderWidth}px`;
+            div.style.borderWidth = `clamp(1px, 2vw, ${innerCircleBorderWidth}px)`;
             div.style.borderStyle = "solid";
             div.style.borderColor = innerCircleBorderColor;
             div.style.borderRadius = "50%";
@@ -43,7 +58,10 @@ function main() {
             innerCircleValue -= margin;
         }
         new ResizeObserver(function () {
-            updateInnerCircleWidth(backgroundCircle, Math.min(backgroundCircle.offsetWidth, backgroundCircle.offsetHeight) - (innerCircleBorderWidth * 2));
+            let value = isMin ?
+                Math.min(backgroundCircle.offsetWidth, backgroundCircle.offsetHeight)
+                : Math.max(backgroundCircle.offsetWidth, backgroundCircle.offsetHeight);
+            updateInnerCircleWidth(backgroundCircle, value - (innerCircleBorderWidth * 2));
         }).observe(backgroundCircle);
     }
 }
@@ -54,11 +72,17 @@ Edge case:
     "inner-circle-border-width-"
     ""
 */
-function getClassValue(elementClassList, prefixClassName, defaultValue) {
-    let value = Array.from(elementClassList).find(className => className.startsWith(prefixClassName));
+function getClassValue(elementClassList, defaultValue, ...prefixClassNames) {
+    let value = Array.from(elementClassList).find(
+        function (className) {
+            for (let prefix of prefixClassNames) {
+                if (className.startsWith(prefix)) return true;
+            }
+            return false;
+        });
 
     if (!value) {
-        value = `${prefixClassName}${defaultValue}`;
+        value = `${prefixClassNames[0]}${defaultValue}`;
     }
     value = getLastElement(value, "-");
     // inner-circle-border-width- this testcase need a second time validation
